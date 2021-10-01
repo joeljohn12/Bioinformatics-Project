27dcb197306cfacd781ea5c7db3593ef5dc86690
ArunJairam <44147242+ArunJairam@users.noreply.github.com>
2021-09-30 21:02:15 -0400

    Update Assignment-2_Part-2.R

--- Assignment-2_Part-2.R
+++ Assignment-2_Part-2.R
@@ -3,7 +3,6 @@ library(ggplot2)
 library(magrittr)
 
 coldata <- read.csv("col_metadata.csv", row.names = 1)
-colnames(coldata)[1] <- gsub('^...','',colnames(coldata)[1])
 
 countData <- read.csv("FinalTable.csv", row.names = 19)
 countData <- countData[-c(1, 6:7, 12:41)]
@@ -11,3 +10,5 @@ countData <- countData[-c(1, 6:7, 12:41)]
 dds <- DESeqDataSetFromMatrix(countData = round(countData),
                               colData = coldata,
                               design = ~ Type)
+vsd <- vst(dds, blind=FALSE)
+plotPCA(vsd, intgroup = c("Type"))
